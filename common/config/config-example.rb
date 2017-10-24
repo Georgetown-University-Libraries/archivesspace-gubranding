0c6cb47085495d9e471a1441a84f79e95aa20abc
Terry W Brady <twb27@georgetown.edu>
2017-10-24 15:46:59 -0700

    redo landing page

---
+++ common/config/config-example.rb
@@ -0,0 +1,6 @@
+# You can override the default configuration options by setting them here.
+AppConfig[:pui_search_results_page_size] = 20
+
+AppConfig[:pui_hide][:digital_objects] = true
+AppConfig[:pui_hide][:accessions] = true
+AppConfig[:pui_hide][:classifications] = true
