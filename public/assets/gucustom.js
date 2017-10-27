878ae35d6305c4fd1ee3f7fe823e60efc48c8f66
Terry W Brady <twb27@georgetown.edu>
2017-10-27 09:21:37 -0700

    year sort

--- public/assets/gucustom.js
+++ public/assets/gucustom.js
@@ -1,11 +1,14 @@
 $(function() {
-	if ($("div.gu-ask-us").is("*")) {
-		var notKie = (window.location.pathname.match(/\/repositories\/7/) == null);
-		if (notKie) {
-			return;
-		}
-		
-		var kie = $("<div class='gu-ask-us'>Please contact the <a href='mailto:bioethics@georgetown.edu'>Bioethics Research Library</a> for assistance with accessing these materials.</div>");
-		$("div.gu-ask-us").replaceWith(kie);
-	}
+    if ($("div.gu-ask-us").is("*")) {
+        var notKie = (window.location.pathname.match(/\/repositories\/7/) == null);
+        if (notKie) {
+            //no action
+        } else {
+            var kie = $("<div class='gu-ask-us'>Please contact the <a href='mailto:bioethics@georgetown.edu'>Bioethics Research Library</a> for assistance with accessing these materials.</div>");
+            $("div.gu-ask-us").replaceWith(kie);
+        }
+    }
+    
+    $("#sort option[value^='year_sort']")
+        .attr("title","A disclaimer about the year sort will go here");
 });
